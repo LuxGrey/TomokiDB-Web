a26fb9754c69cadeadf458791f53b074f733f873
LuxGrey <>
2022-05-06 19:36:15 +0200

    Add README.md
    Also:
    * add Apache Derby dependency in pom.xml
    * rename TomokiDbWeb* classes
    * ignore *.log files

---
+++ README.md
@@ -0,0 +1,13 @@
+# TomokiDB-Web
+
+This is the web-app version of TomokiDB, a tool that can be used to store, organize and query weblinks.
+
+## Requirements
+
+JRE 11 or higher
+
+## Run from source
+
+Run the application with `./mvnw spring-boot:run`
+
+Alternatively, you can build the JAR file with `./mvnw clean package` and then run the JAR file with `java -jar <path-to-JAR-file>`
--- src/main/java/luxgrey/tomokidbweb/TomokiDbWebApplication.java
+++ src/main/java/luxgrey/tomokidbweb/TomokiDbWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TomokidbWebApplication {
+public class TomokiDbWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TomokidbWebApplication.class, args);
+		SpringApplication.run(TomokiDbWebApplication.class, args);
 	}
 
 }
