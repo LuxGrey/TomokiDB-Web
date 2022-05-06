fd6c304809d800ab7f1c956e2cf1fbbe2190ea9c
LuxGrey <>
2022-05-06 10:32:21 +0200

    Initial commit of Spring Initializr generated project

---
+++ src/test/java/luxgrey/tomokidbweb/TomokidbWebApplicationTests.java
@@ -0,0 +1,13 @@
+package luxgrey.tomokidbweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomokidbWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
