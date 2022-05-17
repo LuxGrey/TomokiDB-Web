a2a7eb5776e22e46baa33e683e56f731d379594d
LuxGrey <>
2022-05-17 21:07:01 +0200

    Update frontend build output location
    Frontend now writes build output directly to the directory
    from which the backend serves its content
    
    Also remove backend pom.xml configuration to copy frontend build output

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: path.resolve(__dirname, "../backend/src/main/resources/public"),
+  devServer: {
+    port: 8081,
+    // proxy all dev-server requests starting with /api
+    // to backend (localhost:8080) using http-proxy-middleware
+    // see https://cli.vuejs.org/config/#devserver-proxy
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
 })
