0f59194ff06767a33f2637ebd588039c607f6095
LuxGrey <>
2022-05-20 21:56:03 +0200

    Refactor and add tests to ProfileRepositoryIntegrationTest

--- backend/src/test/java/luxgrey/tomokidbweb/ProfileRepositoryIntegrationTest.java
+++ backend/src/test/java/luxgrey/tomokidbweb/ProfileRepositoryIntegrationTest.java
@@ -1,6 +1,7 @@
 package luxgrey.tomokidbweb;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import luxgrey.tomokidbweb.model.Alias;
 import luxgrey.tomokidbweb.model.Profile;
@@ -31,6 +32,7 @@ public class ProfileRepositoryIntegrationTest {
   public void whenFindAllPaginated_thenReturnPageWithCorrectAmount() {
     // preparation
     final int amountProfiles = 10;
+    final int page = 0;
     final int pageSize = 5;
     List<Profile> profiles = ModelTestHelper.createProfilesWithAliasesAndWeblinks(
         amountProfiles, 2, 3);
@@ -40,7 +42,7 @@ public class ProfileRepositoryIntegrationTest {
     testEntityManager.flush();
 
     // test
-    Pageable pageable = PageRequest.of(0, pageSize);
+    Pageable pageable = PageRequest.of(page, pageSize);
 
     Page<Profile> profilePage = profileRepository.findAll(pageable);
 
@@ -52,6 +54,7 @@ public class ProfileRepositoryIntegrationTest {
   public void whenFindByAliasAndTagIdsPaginated_withoutAliasOrTagIds_thenReturnPageWithCorrectAmount() {
     // preparation
     final int amountProfiles = 10;
+    final int page = 0;
     final int pageSize = 5;
     List<Profile> profiles = ModelTestHelper.createProfilesWithAliasesAndWeblinks(amountProfiles, 2,
         3);
@@ -61,7 +64,7 @@ public class ProfileRepositoryIntegrationTest {
     testEntityManager.flush();
 
     // test
-    Pageable pageable = PageRequest.of(0, pageSize);
+    Pageable pageable = PageRequest.of(page, pageSize);
 
     Page<Profile> profilePage = profileRepository.findByAliasAndTagIds(pageable, null, null);
 
@@ -71,72 +74,162 @@ public class ProfileRepositoryIntegrationTest {
 
   @Test
   public void whenFindByAliasAndTagIdsPaginated_withAliasAndTagIds_thenReturnPageWithCorrectAmount() {
-    // preparation
     final int amountProfiles = 10;
+    final int amountTags = 6;
+    final String expectedAliasName = "SpecialAlias";
+    final int[] profileIndicesWithSoughtAlias = {0, 1, 2, 3,};
+    final int[] soughtTagIndices = {1, 4, 5};
+    final int[] profileIndicesWithSoughtTags = {2, 3, 4, 5};
+    final int page = 0;
     final int pageSize = 5;
-    List<Profile> profiles = ModelTestHelper.createProfilesWithAliasesAndWeblinks(amountProfiles, 2,
-        3);
-    List<Tag> tags = ModelTestHelper.createTags(6);
+    // overlap between Profiles with sought Tags and Profiles with sought Alias
+    final int expectedAmountResults = 2;
+
+    List<Long> soughtTagIds = prepareDatabaseForFindByAliasAndTagIdsTest(
+        amountProfiles,
+        amountTags,
+        expectedAliasName,
+        profileIndicesWithSoughtAlias,
+        soughtTagIndices,
+        profileIndicesWithSoughtTags
+    );
+
+    Pageable pageable = PageRequest.of(page, pageSize);
+
+    Page<Profile> profilePage = profileRepository.findByAliasAndTagIds(
+        pageable, expectedAliasName, soughtTagIds);
+
+    Assert.assertEquals(expectedAmountResults, profilePage.getNumberOfElements());
+    Assert.assertEquals(amountProfiles, profilePage.getTotalElements());
+  }
+
+  @Test
+  public void whenFindByAliasAndTagIsPaginated_withAliasWithoutTagIds_thenReturnPageWithCorrectAmount() {
+    final int amountProfiles = 10;
+    final int amountTags = 0;
+    final String expectedAliasName = "SpecialAlias";
+    final int[] profileIndicesWithSoughtAlias = {0, 1, 2, 3,};
+    final int[] soughtTagIndices = {};
+    final int[] profileIndicesWithSoughtTags = {};
+    final int page = 0;
+    final int pageSize = 5;
+    // Profiles with sought Alias
+    final int expectedAmountResults = 4;
+
+    prepareDatabaseForFindByAliasAndTagIdsTest(
+        amountProfiles,
+        amountTags,
+        expectedAliasName,
+        profileIndicesWithSoughtAlias,
+        soughtTagIndices,
+        profileIndicesWithSoughtTags
+    );
+
+    Pageable pageable = PageRequest.of(page, pageSize);
+
+    Page<Profile> profilePage = profileRepository.findByAliasAndTagIds(
+        pageable, expectedAliasName, null);
+
+    Assert.assertEquals(expectedAmountResults, profilePage.getNumberOfElements());
+    Assert.assertEquals(amountProfiles, profilePage.getTotalElements());
+  }
+
+  @Test
+  public void whenFindByAliasAndTagIdsPaginated_withoutAliasWithTagIds_thenReturnPageWithCorrectAmount() {
+    final int amountProfiles = 10;
+    final int amountTags = 6;
+    final String expectedAliasName = null;
+    final int[] profileIndicesWithSoughtAlias = {};
+    final int[] soughtTagIndices = {1, 4, 5};
+    final int[] profileIndicesWithSoughtTags = {2, 3, 4, 5};
+    final int page = 0;
+    final int pageSize = 5;
+    // overlap between Profiles with sought Tags and Profiles with sought Alias
+    final int expectedAmountResults = 4;
+
+    List<Long> soughtTagIds = prepareDatabaseForFindByAliasAndTagIdsTest(
+        amountProfiles,
+        amountTags,
+        expectedAliasName,
+        profileIndicesWithSoughtAlias,
+        soughtTagIndices,
+        profileIndicesWithSoughtTags
+    );
+
+    Pageable pageable = PageRequest.of(page, pageSize);
+
+    Page<Profile> profilePage = profileRepository.findByAliasAndTagIds(
+        pageable, expectedAliasName, soughtTagIds);
+
+    Assert.assertEquals(expectedAmountResults, profilePage.getNumberOfElements());
+    Assert.assertEquals(amountProfiles, profilePage.getTotalElements());
+  }
+
+  /**
+   * Sets up the database with data that are relevant for tests which test
+   * findByAliasAndTagIds(...)
+   *
+   * WARNING: Ensure that you provide the indices-parameters with arguments that don't cause
+   * Index out of bounds Exceptions; amountProfiles and amountTags parameters determine the sizes
+   * of the respective collections
+   *
+   * @param amountProfiles                number of profiles to generate
+   * @param amountTags                    number of tags to generate
+   * @param soughtAliasName               name of the Alias to assign to selected Profiles
+   * @param profileIndicesWithSoughtAlias indices of the collection with generated Profiles where a
+   *                                      sought Alias should be associated
+   * @param soughtTagIndices              indices of the collection with generated Tags that that
+   *                                      should be associated with selected Profiles
+   * @param profileIndicesWithSoughtTags  indices of the collection with gennerated Profiles where
+   *                                      the sought Tags should be associated
+   * @return a list of IDs of the sought Tags that were specified by soughtTagIndices
+   * @see ProfileRepository#findByAliasAndTagIds(Pageable, String, Collection)
+   */
+  private List<Long> prepareDatabaseForFindByAliasAndTagIdsTest(
+      int amountProfiles,
+      int amountTags,
+      String soughtAliasName,
+      int[] profileIndicesWithSoughtAlias,
+      int[] soughtTagIndices,
+      int[] profileIndicesWithSoughtTags) {
+    List<Profile> profiles = ModelTestHelper.createProfilesWithAliasesAndWeblinks(
+        amountProfiles, 2, 3);
+    List<Tag> tags = ModelTestHelper.createTags(amountTags);
+
     for (Tag t : tags) {
       // persist earlier to assign IDs
       testEntityManager.persist(t);
     }
 
-    final String expectedAliasName = "CustomAlias";
-    // "Tag1, "Tag3", "Tag4"
-    final int[] expectedTagsIndices = {0, 2, 3};
-    final int[] expectedProfilesIndices = {1, 8};
-    final int expectedAmountResults = expectedProfilesIndices.length;
-
-    // set expected Tag IDs
-    List<Long> expectedTagIds = new ArrayList<>();
-    for (int k : expectedTagsIndices) {
-      expectedTagIds.add(tags.get(k).getId());
+    // get IDs of sought Tags
+    List<Long> soughtTagIds = new ArrayList<>();
+    for(int l : soughtTagIndices) {
+      soughtTagIds.add(tags.get(l).getId());
     }
 
-    // set expected result Profiles
-    for (int i : expectedProfilesIndices) {
-      Profile currentProfile = profiles.get(i);
-
-      Alias customAlias = new Alias();
-      // bring some variation into the expected Aliases
-      customAlias.setName(expectedAliasName + i);
-      currentProfile.getAliases().add(customAlias);
-
-      for (int k : expectedTagsIndices) {
-        Tag currentTag = tags.get(k);
-
-        currentProfile.getTags().add(currentTag);
+    // assign sought Aliases
+    if(soughtAliasName != null) {
+      for (int i : profileIndicesWithSoughtAlias) {
+        Alias alias = new Alias();
+        alias.setName(soughtAliasName + i);
+        profiles.get(i).getAliases().add(alias);
       }
     }
 
-    // set Profile with expected Tags, but without expected Alias
-    Profile unexpectedProfile = profiles.get(2);
-    for (int k : expectedTagsIndices) {
-      Tag currentTag = tags.get(k);
-
-      unexpectedProfile.getTags().add(currentTag);
+    // assing sought Tags
+    for (int j : profileIndicesWithSoughtTags) {
+      Profile profile = profiles.get(j);
+      for (int k : soughtTagIndices) {
+        profile.getTags().add(tags.get(k));
+      }
     }
 
-    // set Profile with expected Alias, but only 1 of the expected Tags
-    unexpectedProfile = profiles.get(4);
-    Alias unexpectedProfileAlias = new Alias();
-    // using profiles.size() to get a suffix that can not appear for any of the Aliases of the expected result profiles
-    unexpectedProfileAlias.setName(expectedAliasName + profiles.size());
-    unexpectedProfile.getAliases().add(unexpectedProfileAlias);
-
+    // persist Profiles
     for (Profile p : profiles) {
       testEntityManager.persist(p);
     }
     testEntityManager.flush();
 
-    // test
-    Pageable pageable = PageRequest.of(0, pageSize);
-
-    Page<Profile> profilePage = profileRepository.findByAliasAndTagIds(pageable, expectedAliasName,
-        expectedTagIds);
-
-    Assert.assertEquals(expectedAmountResults, profilePage.getNumberOfElements());
-    Assert.assertEquals(amountProfiles, profilePage.getTotalElements());
+    return soughtTagIds;
   }
 }
